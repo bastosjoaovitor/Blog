60bec81852bbcd9ea9861c184c742c9d6fde87dc
João Vitor Bastos <156615917+bastosjoaovitor@users.noreply.github.com>
2024-03-06 19:14:53 -0300

    Finished cards and their color codes
    I finished the cards for part 1 and 2 of the main one, I added a script code to change the color of the image in part 1 without changing the color of the letters

---
+++ Scripts/script.js
@@ -0,0 +1,23 @@
+document.getElementById('div-img-1').addEventListener('mouseover', function() {
+    document.getElementById('img-1').style.filter = 'brightness(65%)';
+});
+
+document.getElementById('div-img-1').addEventListener('mouseout', function() {
+    document.getElementById('img-1').style.filter = 'brightness(50%)';
+});
+
+document.getElementById('div-img-2').addEventListener('mouseover', function() {
+    document.getElementById('img-2').style.filter = 'brightness(50%)';
+});
+
+document.getElementById('div-img-2').addEventListener('mouseout', function() {
+    document.getElementById('img-2').style.filter = 'brightness(40%)';
+});
+
+document.getElementById('div-img-3').addEventListener('mouseover', function() {
+    document.getElementById('img-3').style.filter = 'brightness(50%)';
+});
+
+document.getElementById('div-img-3').addEventListener('mouseout', function() {
+    document.getElementById('img-3').style.filter = 'brightness(100%)';
+});
